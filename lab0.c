2155765c57d565ae93821fa467d6858feba4845e
Allan <allanhuidobro@gmail.com>
2020-01-27 14:09:56 -0800

    main

---
+++ lab0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int readThemChars(char c)
+{
+    
+}
+
+int main(int argc, char** argv)
+{
+    //check for command line arguments
+
+    //if "-" || or empty, stdin
+
+    //reading loop
+}
