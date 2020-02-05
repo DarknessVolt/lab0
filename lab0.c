8d83b8d62b261f80cbb92f2cc6ad8e878426bbcd
Allan Huidobro <allanhuidobro@gmail.com>
2020-02-05 15:05:07 -0800

    Update lab0

--- lab0.c
+++ lab0.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
-
-int readThemChars(char c)
-{
-    
-}
+#include <math.h>
 
 int main(int argc, char** argv)
 {
     //check for command line arguments
-
-    //if "-" || or empty, stdin
-
-    //reading loop
+    if(argc == 1 || argv[1] == '-')
+    {
+        //use stdin
+    }
+    else
+    {
+        //open thee file
+    }
+    int i;
+    int j;
+    int parity;
+    char curr;
+    int dec;
+    //loop read 8 bits
+    while(/*end condition*/)
+    {
+        for(i = 0; i < 8; i++)
+        {
+            //save "bit" to string
+            
+            //calculate the shtuffs
+            if(curr == '1')
+            {
+                dec += pow(2, i);
+                parity += 1;
+            }
+        }
+        //check if there are any more chars+
+        //read a char
+        //add char to string
+        //convert to int
+        //print and formatting
+    }
+    //
 }
