0a83ed54480c00fdf132cf0337c6325b2db52257
Allan Huidobro <allanhuidobro@gmail.com>
2020-02-11 13:44:27 -0800

    restructure of the program

--- lab0.c
+++ lab0.c
@@ -1,34 +1,100 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
 
-char *fileName;
+#define STR_LEN 200
 
-//read input
-int readInput(char* in)
+typedef struct out
 {
-    int pos = 0;
+    int decimal;
+    int parity;
+    char c;
+    char byte[8];
+}Out;
+
+char readChar(int* offset, char* stringOfBits)
+{
+    char c = stringOfBits[*offset];
+    *offset++;
+    return c;
+}
+
+int readBit(int* offset, char* stringOfBits)
+{
+    char c;
+    do
+    {
+        c = readChar(offset, stringOfBits);
+
+        if(c == '1' || c == '0')
+        {
+            return (int)(c - '0');
+        }
+        else if(c == '\0')
+        {
+            return -1;
+        }
+
+    }while(*offset < STR_LEN);
+
+    return -1;
+}
+
+Out readByte(int* offset, char* stringOfBits)
+{
+    int i;
+    int bit;
+    Out out;
     
-    while(1)
+    out.decimal = 0;
+    out.parity = 0;
+    
+    for(i = 7; i >= 0; i++)
     {
 
+        bit = readBit(offset, stringOfBits);
+
+        if(bit == -1 && i == 7)
+        {
+            out.decimal = -1;
+            return out;
+        }
+        else if(bit == -1)
+        {
+            for(i; i>=0; i++)
+            {
+                out.byte[7-i] = '0';
+            }
+        }
+        else
+        {
+            out.parity += bit;
+            out.decimal += bit * pow(2, i);
+            out.byte[7-i] = bit + '0';
+        }
     }
+    out.c = (char)out.decimal;
+    return out;
 }
 
 int main(int argc, char** argv)
 {
-    char *fileName = malloc(10*sizeof(char));
+    char *fileName = malloc(100*sizeof(char));
+    char stringOfBits[STR_LEN];
+    int *offset;
     //check for command line arguments
-    if(argc == 1 || argv[1] == "-")
+    if(argc == 1 || strcmp(argv[1], "-"))
     {
         //use stdin
-        readInput(fileName);
+        
     }
     else
     {
-        fileName = argv[1];
+        strcpy(fileName, argv[1]);
     }
 
     //open thee file
@@ -39,22 +105,50 @@ int main(int argc, char** argv)
         printf("ERROR: File Not Found");
         exit(1);
     }
-
-    int i;
+    
+    int i = 0;
     int j;
-    int parity;
-    char curr;
-    int dec;
-    int *stringOfBits = calloc(8, sizeof(char));
+    Out out;
 
-    int readReturn = 1;
+    int loop = 1;
+    *offset = 0;
+
+    printf("Original ASCII    Decimal  Parity  \n");
+    printf("-------- -------- -------- --------\n");
     //loop read 8 bits
+    while(loop)
+    {
+        out = readByte(offset, stringOfBits);
+
+        if(out.decimal = -1)
+        {
+            break;
+        }
+        else
+        {
+            printf("%s %8c %8d ", out.byte, out.c, out.decimal);
+            if(out.parity%2)
+            {
+                printf("ODD\n");
+            }
+            else
+            {
+                printf("EVEN\n");
+            }
+            
+        }
+    }
+    /*
     while(readReturn)
     {
         while(i < 8)
         {
             //save "bit" to string
             readReturn = (int)read(file, &curr, 1);
+            if(curr == ' ' || curr == '\n' || curr == '\t' || curr ='\0')
+            {
+                continue;
+            }
             if(readReturn == 0)
             {
                 for(i; i < 8; i++)
@@ -74,7 +168,7 @@ int main(int argc, char** argv)
         }
         //print and formatting
         printf("%s %8c %8d %8s", stringOfBits, (char)dec, dec, parity);
-    }
+    }*/
     //
     return 0;
 }
