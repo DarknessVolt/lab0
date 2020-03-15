f7362c4949606585bfe5a42b4821a3bfdbce0c2b
Allan <allanhuidobro@gmail.com>
2020-03-15 16:37:12 -0700

    launch completed?

---
+++ lab2/README.md
@@ -0,0 +1,4 @@
+# lab 2
+
+Launch is pretty straight forward.
+Launch the program specified in the command line via forking and exec and report some stuff, so I made a method that handles the forking, and another that handles preparing argv for the program that will be launched in the child. This will hopefully give me some leverage that'll help make tube.c easier to make.
--- lab2/launch.c
+++ lab2/launch.c
@@ -2,24 +2,48 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
+
+char** prepArgv(int argc, char** argv)
+{
+    char* argv2[argc];
+    int i;
+
+    for(i = 1; i < argc; i++)
+    {
+        strcpy(argv2[i-1], argv[i]);
+    }
+    argv2[argc-1] = (char*)0;
+    
+    return argv2;
+}
 
 int forkAndLaunch(int argc, char** argv, pid_t pid)
 {
     if(pid == 0)
     {
         //child process
-        execve()
+        
+        execve(argv[1], prepArgv(argc, argv), NULL);
+
+        printf("EXEC ERROR");
     }
     else if(pid < 0)
     {
-        
         //error
         printf("FORKING ERROR")
     }
     else
     {
         //parent process
-        printf("%s: %d", argv[1], pid);
+
+        int status;
+
+        printf("%s: $$ = %d", argv[1], pid);
+
+        waitpid(pid, &status, 0);
+
+        printf("%s: $? = %d", argv[1], status);
         
     }
 }
