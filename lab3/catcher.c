a2a1cfe20aa30b58c7fce9dcd97998106d67d037
Allan Huidobro <allanhuidobro@gmail.com>
2020-03-23 19:02:46 -0700

    bugfixes

--- lab3/catcher.c
+++ lab3/catcher.c
@@ -6,12 +6,11 @@
 
 
 int maxTermCount = 3;
-static int totalSignals = 64;
 
-enum signals{
-SIGHU = 1, SIGINT, SIGQUIT, SIGILL, SIGTRAP,
-SIGABRT, SIGBUS, SIGFPE, SIGKILL, SIGUSR1,
-SIGSEGV, SIGUSR2, SIGPIPE, SIGALRM, SIGTERM,
+/*enum signals{
+SIGHU = 1, SIGINT_, SIGQUIT, SIGILL_, SIGTRAP,
+SIGABRT_, SIGBUS, SIGFPE_, SIGKILL, SIGUSR1,
+SIGSEGV_, SIGUSR2, SIGPIPE, SIGALRM, SIGTERM_,
 SIGSTKFLT, SIGCHLD, SIGCONT, SIGSTOP, SIGTSTP,
 SIGTTIN, SIGTTOU, SIGURG, SIGXCPU, SIGXFSZ,
 SIGVTALRM, SIGPROF, SIGWINCH, SIGIO, SIGPWR,
@@ -22,28 +21,28 @@ SIGRTMIN14, SIGRTMIN15, SIGRTMAX14, SIGRTMAX13, SIGRTMAX12,
 SIGRTMAX11, SIGRTMAX10, SIGRTMAX9, SIGRTMAX8, SIGRTMAX7,
 SIGRTMAX6, SIGRTMAX5, SIGRTMAX4, SIGRTMAX3, SIGRTMAX2,
 SIGRTMAX1, SIGRTMAX
-};
+};*/
 
-static char csignals[totalSignals][11] = {
-"SIGHU", "SIGINT", "SIGQUIT", "SIGILL", "SIGTRAP",
-"SIGABRT", "SIGBUS", "SIGFPE", "SIGKILL", "SIGUSR1",
-"SIGSEGV", "SIGUSR2", "SIGPIPE", "SIGALRM", "SIGTERM",
-"SIGSTKFLT", "SIGCHLD", "SIGCONT", "SIGSTOP", "SIGTSTP",
-"SIGTTIN", "SIGTTOU", "SIGURG", "SIGXCPU", "SIGXFSZ",
-"SIGVTALRM", "SIGPROF", "SIGWINCH", "SIGIO", "SIGPWR",
-"SIGSYS", "SIGRTMIN", "SIGRTMIN+1", "SIGRTMIN+2", "SIGRTMIN+3",
-"SIGRTMIN+4", "SIGRTMIN+5", "SIGRTMIN+6", "SIGRTMIN+7", "SIGRTMIN+8",
-"SIGRTMIN+9", "SIGRTMIN+10", "SIGRTMIN+11", "SIGRTMIN+12", "SIGRTMIN+13",
-"SIGRTMIN+14", "SIGRTMIN+15", "SIGRTMAX-14", "SIGRTMAX-13", "SIGRTMAX-12",
-"SIGRTMAX-11", "SIGRTMAX-10", "SIGRTMAX-9", "SIGRTMAX-8", "SIGRTMAX-7",
-"SIGRTMAX-6", "SIGRTMAX-5", "SIGRTMAX-4", "SIGRTMAX-3", "SIGRTMAX-2",
-"SIGRTMAX-1", "SIGRTMAX"
+static char csignals[64][12] = {
+"HU", "INT", "QUIT", "ILL", "TRAP",
+"ABRT", "BUS", "FPE", "KILL", "USR1",
+"SEGV", "USR2", "PIPE", "ALRM", "TERM",
+"STKFLT", "CHLD", "CONT", "STOP", "TSTP",
+"TTIN", "TTOU", "URG", "XCPU", "XFSZ",
+"VTALRM", "PROF", "WINCH", "IO", "PWR",
+"SYS", "RTMIN", "RTMIN+1", "RTMIN+2", "RTMIN+3",
+"RTMIN+4", "RTMIN+5", "RTMIN+6", "RTMIN+7", "RTMIN+8",
+"RTMIN+9", "RTMIN+10", "RTMIN+11", "RTMIN+12", "RTMIN+13",
+"RTMIN+14", "RTMIN+15", "RTMAX-14", "RTMAX-13", "RTMAX-12",
+"RTMAX-11", "RTMAX-10", "RTMAX-9", "RTMAX-8", "RTMAX-7",
+"RTMAX-6", "RTMAX-5", "RTMAX-4", "RTMAX-3", "RTMAX-2",
+"RTMAX-1", "RTMAX"
 };
 
 int termCount = 0;
 int sigCount = 0;
 
-void sigHandler(int sig)
+void sighandler(int sig)
 {
     //add to termcount
     if(sig == SIGTERM)
@@ -56,9 +55,11 @@ void sigHandler(int sig)
     }
 
     sigCount++;
-
     //report signal caught
-    printf("%s caught at %d\n", sig, time());
+    printf("SIG%s caught at %d\n", csignals[sig-1], time(NULL));
+
+    //reset signal
+    signal(sig, sighandler);
 }
 
 int main(int argc, char** argv)
@@ -77,29 +78,30 @@ int main(int argc, char** argv)
 
     for(i = 1; i < argc; i++)
     {
-        for(j = SIGHU; j < SIGRTMAX, j++)
+        for(j = 1; j < 64; j++)
         {
-            if(strcmp(csignals[j-1], argc[i]) == 0)
+            if(strcmp(csignals[j-1], argv[i]) == 0)
             {
                 sig[i-1] = j;
+                //printf("%d\n", sig[i-1]);
                 break;
             }
         }
     }
 
+    //register handler for each arg
+    for(i = 0; i < sigSize; i++)
+    {
+        signal(sig[i], sighandler);
+    }
     //report pid to stderr
     fprintf(stderr, "catcher: $$ = %d\n", getpid());
 
     while(keepLooping)
     {
-        //register handler for each arg
-        for(i = 0; i < sigSize; i++)
-        {
-            singal(sig[i], sigHandler);
-        }
         //pause
         pause();
-
+        //printf("yeet");
         //if 3 successive SIGTERM, terminate loop
         if(termCount >= maxTermCount)
         {
