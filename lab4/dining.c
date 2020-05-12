d2783c5dc29e6ad8054684b42bc4e7aac8b1fd33
Allan Huidobro <allanhuidobro@gmail.com>
2020-05-11 18:18:13 -0700

    MMm, delishous semaphores

--- lab4/dining.c
+++ lab4/dining.c
@@ -3,22 +3,23 @@
 #include <semaphore.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <time.h>
 #include <signal.h>
 #include <sys/mman.h>
 
 
-#define SEM_CHOPSTICKS "/actuallyFork"
+#define SEM_CHOPSTICKS "/actuallyForks1"
 
 int RAND_UPPER = 1000000;
 
 int cycles = 0;
 int loop = 1;
 
-int seats;
-int philNum;
-int numOfForks;
+int seats = 0;
+int philNum = 0;
+int numOfChopsticks;
 
 void sighandler(int sig)
 {
@@ -56,65 +57,76 @@ int main(int argc, char** argv)
     int i;
 
     //process args
-    sscanf(argv[1], "%d", seats);
-    sscanf(argv[2], "%d", philNum);
-    numOfForks = seats-1;
+    sscanf(argv[1], "%d", &seats);
+    sscanf(argv[2], "%d", &philNum);
 
-    //initialize shared memory
-    int fd = shm_open(SEM_CHOPSTICKS, O_RDWR | O_CREAT | O_EXCL, 0664);
-    if(fd == EEXIST)
+    numOfChopsticks = seats;
+
+    int fd = shm_open(SEM_CHOPSTICKS, O_RDWR | O_CREAT, 0664);
+
+    if(fd == -1)
     {
-        PERROR(NULL);
-        fd = shm_open(SEM_CHOPSTICKS, O_RDWR, 0664);
+        printf("shm_open Failed");
+        return 1;
     }
+    ftruncate(fd, sizeof(sem_t));
 
-    ftruncate(fd, seats*sizeof(sem_t));
+    sem_t* chopsticks = mmap(NULL, seats*sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    
 
-    sem_t* forks = (sem_t*)mmap(NULL, numOfForks*sizeof(sem_t), PROT_EXEC | PROT_WRITE, MAP_SHARED, fd, 0);
-
+    //sem_t* chopstick_address = (sem_t*)calloc(numOfChopsticks, sizeof(sem_t));
+    //
+    //printf("yeet1\n");
     /*if(forks == MAP_FAILED)
     {
         fprintf(stderr, "Map failed");
         return 1;
     }*/
 
-    for(i = 0; i < numOfForks; i++)
+
+    for(i = 0; i < seats; i++)
     {
-        sem_init(&forks[i], 0666, 0);
+        
+        //&chopstick_address[i] = chopsticks + i;
+        //printf("yeet%d %d\n", i, chopsticks[i]);
+        sem_init(&chopsticks[i], 0666, 1);
     }
     
-    
+    printf("yeetsuccess\n");
     
     signal(SIGTERM, sighandler);
 
     
-
     int cycles = 0;
 
     do
     {
-        sem_wait(&forks[philNum]);
-        sem_wait(&forks[(philNum+1) % numOfForks]);
-
+        //printf("Loop %d, %d", );
+        if(loop)
+        {
+            sem_wait(&chopsticks[philNum]);
+            sem_wait(&chopsticks[(philNum+1) % numOfChopsticks]);
+        }
         //use eat
         eat(i);
 
-        sem_post(&forks[philNum]);
-        sem_post(&forks[(philNum+1) % numOfForks]);
-
+        if(loop)
+        {
+            sem_post(&chopsticks[philNum]);
+            sem_post(&chopsticks[(philNum+1) % numOfChopsticks]);
+        }
         //use think
         think(i);
 
         cycles++;
 
     }while(loop);
-
     
     
-    
-    for(i = 0; i < numOfForks; i++)
+    for(i = 0; i < seats; i++)
     {
-        sem_destroy(&forks[i]);
+        sem_destroy(&chopsticks[i]);
     }
-    return 1;
+    shm_unlink(SEM_CHOPSTICKS);
+    return 0;
 }
